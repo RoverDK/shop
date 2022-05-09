25b34f06e151a717fbea21ec3116c3835ae336b2
Danila Karunny <1610163d@gmail.com>
2022-05-09 09:39:18 +0300

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import {MainLayout} from './MainLayout';
 
-import './styles/zeroStyles.css'
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <MainLayout />
